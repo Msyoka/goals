d4d7adbb899db4e4ff5d3a8e2c5fb92e49c4e561
Msyoka <daviemumo37@gmail.com>
2021-02-04 10:57:57 +0300

    create goal class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'goals';
+  goals:string[];
+
+  constructor(){
+    this.goals = ['Watch Finding Nemo', 'Cook lunch', 'Get new phone']
+  }
 }
