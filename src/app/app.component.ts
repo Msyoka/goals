02214a4007e4b08e109e75c8fdc6e6124c04df1e
Msyoka <daviemumo37@gmail.com>
2021-02-04 11:45:29 +0300

    create goal-detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,6 @@ import { Goal } from './goal'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals:Goal[] = [
-    {id:1, name:'Watch finding nemo'},
-    {id:2,name:'Buy cookies'},
-    {id:3,name:'Get new phone'},
-    {id:4,name:'Get Dog Food'},
-    {id:5,name:'Solve math homework'},
-    {id:6,name:'Plot my world domination plan'},
-
-  ]
+  
 
 }
